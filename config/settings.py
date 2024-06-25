5934e97189ac28c151aefd5e03af8a77d1fb7c0c
Moein Naghibirad <radmoein4@gmail.com>
2024-06-26 01:14:40 +0430

    fix: missing rest setting

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,11 @@ MEDIA_URL = "media/"
 
 # User models setting
 AUTH_USER_MODEL = 'account.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
