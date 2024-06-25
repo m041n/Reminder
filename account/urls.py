13e8866a6fd10b75ca474b62c91b5cc8e56a3f26
Moein Naghibirad <radmoein4@gmail.com>
2024-06-26 00:53:17 +0430

    feat: Add jwt url in account app

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,16 @@
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from django.urls import path
 
 from . import views
 
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
+
     path('register/', views.RegisterView.as_view())
 ]
