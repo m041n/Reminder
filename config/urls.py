d9a369b31d2af2a32a96f23f0dfc4abf7c9dc3a3
Moein Naghibirad <radmoein4@gmail.com>
2024-06-26 01:33:04 +0430

    feat: List all persons

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('reminder/', include('reminder.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- reminder/views.py
+++ reminder/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+
+from account.models import Profile
+from .models import Person
+from . import serializers
+
+
+class PersonView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        profile = get_object_or_404(Profile, user_rel=request.user)
+        persons = Person.objects.filter(person_profile_rel=profile)
+        if persons.exists():
+            serializer = serializers.PersonSerailizers(instance=persons, many=True)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(data={"message": "Persons not exists"}, status=status.HTTP_404_NOT_FOUND)
